c0530fcf321ff6e548a1380ae32354ae7779d75b
h44343880 <h0920185003@gmail.com>
2022-09-24 23:40:28 +0800

    change "for loop" variables' name

--- 410410027_hw1.c
+++ 410410027_hw1.c
@@ -11,22 +11,22 @@ int main(){
     scanf("%d%d%d", &nodes, &links, &requests);// 讀入node數, link數和request數
     Node * node_arr = (Node*)malloc(sizeof(Node) * nodes);// 建立node的array
     int id, mem_cnt;
-    for(int i = 0; i < nodes; i++){// 讀入每個node的memory數
+    for(int cnt = 0; cnt < nodes; cnt++){// 讀入每個node的memory數
         scanf("%d%d", &id, &mem_cnt);
         node_arr[id].mem_cnt = mem_cnt;
         node_arr[id].channel = (int*)calloc(nodes,sizeof(int));// 為每個node建立channel的array
     }
-    int first, second, cnt;
-    for(int i = 0; i < links; i++){// 讀入node之間的channel數
-        scanf("%d%d%d%d", &id, &first, &second, &cnt);
-        node_arr[first].channel[second] = cnt;
-        node_arr[second].channel[first] = cnt; 
+    int first, second, channel_num;
+    for(int cnt = 0; cnt < links; cnt++){// 讀入node之間的channel數
+        scanf("%d%d%d%d", &id, &first, &second, &channel_num);
+        node_arr[first].channel[second] = channel_num;
+        node_arr[second].channel[first] = channel_num; 
     }
 
     int * req_start = (int*)malloc(sizeof(int) * requests);
     int * req_end = (int*)malloc(sizeof(int) * requests);
-    for(int i = 0; i < requests; i++){// 讀入request
-        scanf("%d%d%d", &id, &req_start[i], &req_end[i]);
+    for(int cnt = 0; cnt < requests; cnt++){// 讀入request
+        scanf("%d%d%d", &id, &req_start[cnt], &req_end[cnt]);
     }
 
     int * queue = (int*)malloc(sizeof(int) * nodes);// 建立queue
@@ -40,34 +40,34 @@ int main(){
     int * accept_arr = (int*)calloc(requests, sizeof(int));// 紀錄哪些request為accepted
     int accept_cnt = 0;// 紀錄accept request的數量    
 
-    for(int i = 0; i < requests; i++){
+    for(int cnt = 0; cnt < requests; cnt++){
         // 初始化
         front = -1;
         back = -1;
-        for(int j = 0; j < nodes; j++){
-            color_arr[j] = 'w';
-            predecessor_arr[j] = -1;
-            length_arr[j] = 0;
+        for(int cnt2 = 0; cnt2 < nodes; cnt2++){
+            color_arr[cnt2] = 'w';
+            predecessor_arr[cnt2] = -1;
+            length_arr[cnt2] = 0;
         }
 
         // 執行bfs找出shortest path
-        queue[0] = req_start[i];// 將起點node推入queue中
+        queue[0] = req_start[cnt];// 將起點node推入queue中
         color_arr[queue[0]] = 'g';
         back++;
         while(front != back){
-            for(int j = 0; j < nodes; j++){
-                if(node_arr[queue[front + 1]].channel[j]){
-                    if(color_arr[j] == 'w'){
-                        color_arr[j] = 'g';
-                        length_arr[j] = length_arr[queue[front + 1]] + 1;
-                        predecessor_arr[j] = queue[front + 1];
-                        queue[++back] = j;
-                    }else if(color_arr[j] == 'g'){
-                        if(length_arr[j] == length_arr[queue[front + 1]] + 1){// 若到達下一個Node的路徑長一樣，則比較兩條路徑的資源多寡，取資源較多者的路徑
-                            int min_resource1 = (node_arr[predecessor_arr[j]].channel[j] < (node_arr[predecessor_arr[j]].mem_cnt)) ? node_arr[predecessor_arr[j]].channel[j] : (node_arr[predecessor_arr[j]].mem_cnt);
-                            int min_resource2 = (node_arr[queue[front + 1]].channel[j] < node_arr[queue[front + 1]].mem_cnt) ? node_arr[queue[front + 1]].channel[j] : node_arr[queue[front + 1]].mem_cnt;
+            for(int cnt2 = 0; cnt2 < nodes; cnt2++){
+                if(node_arr[queue[front + 1]].channel[cnt2]){
+                    if(color_arr[cnt2] == 'w'){
+                        color_arr[cnt2] = 'g';
+                        length_arr[cnt2] = length_arr[queue[front + 1]] + 1;
+                        predecessor_arr[cnt2] = queue[front + 1];
+                        queue[++back] = cnt2;
+                    }else if(color_arr[cnt2] == 'g'){
+                        if(length_arr[cnt2] == length_arr[queue[front + 1]] + 1){// 若到達下一個Node的路徑長一樣，則比較兩條路徑的資源多寡，取資源較多者的路徑
+                            int min_resource1 = (node_arr[predecessor_arr[cnt2]].channel[cnt2] < (node_arr[predecessor_arr[cnt2]].mem_cnt)) ? node_arr[predecessor_arr[cnt2]].channel[cnt2] : (node_arr[predecessor_arr[cnt2]].mem_cnt);
+                            int min_resource2 = (node_arr[queue[front + 1]].channel[cnt2] < node_arr[queue[front + 1]].mem_cnt) ? node_arr[queue[front + 1]].channel[cnt2] : node_arr[queue[front + 1]].mem_cnt;
                             if(min_resource1 < min_resource2)
-                            predecessor_arr[j] = queue[front + 1];
+                            predecessor_arr[cnt2] = queue[front + 1];
                         }
                     }
                 }
@@ -75,22 +75,22 @@ int main(){
             color_arr[queue[front + 1]] = 'b';
             front++;
         }
-        if(predecessor_arr[req_end[i]] != -1){// 判斷是否有路徑通往終點，有的話才執行if內的程式碼
+        if(predecessor_arr[req_end[cnt]] != -1){// 判斷是否有路徑通往終點，有的話才執行if內的程式碼
             int path_len = 1;
-            int current = req_end[i];
-            while(current != req_start[i]){// 計算path的長度
+            int current = req_end[cnt];
+            while(current != req_start[cnt]){// 計算path的長度
                 path_len++;
                 current = predecessor_arr[current];
             }
             int * path = (int*)malloc(sizeof(int) * nodes);
-            for(int j = 0; j < nodes; j++){
-                path[j] = -1;
+            for(int cnt2 = 0; cnt2 < nodes; cnt2++){
+                path[cnt2] = -1;
             }
 
             // 檢查shortest path可不可行
             int accept = 1;
-            current = predecessor_arr[req_end[i]];// end的前一個node
-            if(node_arr[req_end[i]].mem_cnt == 0 || node_arr[req_start[i]].mem_cnt == 0){
+            current = predecessor_arr[req_end[cnt]];// 將current設為end的前一個node
+            if(node_arr[req_end[cnt]].mem_cnt == 0 || node_arr[req_start[cnt]].mem_cnt == 0){ // 若起點或終點沒有memory，不可能accept
                 accept = 0;
             }else{
                 while(predecessor_arr[current] != -1){
@@ -102,11 +102,11 @@ int main(){
                 }
             }
             if(accept){
-                accept_arr[i] = 1;
+                accept_arr[cnt] = 1;// 表示該request accept
                 accept_cnt++;
-                current = req_end[i];
+                current = req_end[cnt];
                 path[path_len - 1] = current;
-                while(current != req_start[i]){
+                while(current != req_start[cnt]){// 扣掉path用掉的資源
                     node_arr[current].mem_cnt--;
                     node_arr[predecessor_arr[current]].mem_cnt--;
                     node_arr[current].channel[predecessor_arr[current]]--;
@@ -115,18 +115,18 @@ int main(){
                     path_len--;
                     path[path_len - 1] = current;
                 }
-                path_arr[i] = path;  
+                path_arr[cnt] = path;  
             }
         }
     }
     // 輸出結果
     printf("%d\n", accept_cnt);
-    for(int i = 0; i < requests; i++){
-        if(accept_arr[i]){
-            printf("%d ", i);
-            for(int j = 0; j < nodes; j++){
-                if(path_arr[i][j] != -1)
-                    printf("%d ", path_arr[i][j]);
+    for(int cnt = 0; cnt < requests; cnt++){
+        if(accept_arr[cnt]){
+            printf("%d ", cnt);
+            for(int cnt2 = 0; cnt2 < nodes; cnt2++){
+                if(path_arr[cnt][cnt2] != -1)
+                    printf("%d ", path_arr[cnt][cnt2]);
                 else
                     break;
             }
