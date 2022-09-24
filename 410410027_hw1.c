d7d3a6e94b9adb6b28cb73ff82411626b117270e
h44343880 <h0920185003@gmail.com>
2022-09-24 17:51:02 +0800

    adjust resource weight

--- 410410027_hw1.c
+++ 410410027_hw1.c
@@ -2,43 +2,42 @@
 #include<stdlib.h>
 
 typedef struct Node{
-    int id;
-    int mem_cnt;
-    int * channel;
+    int mem_cnt;// 此node的memory數量
+    int * channel;// 紀錄與其他node之間channel數量的array
 }Node;
 
 int main(){
-    // 讀入資料
     int nodes, links, requests;
-    scanf("%d%d%d", &nodes, &links, &requests);
-    Node * node_arr = (Node*)malloc(sizeof(Node) * nodes);// 建立儲存所有node資訊的array
+    scanf("%d%d%d", &nodes, &links, &requests);// 讀入node數, link數和request數
+    Node * node_arr = (Node*)malloc(sizeof(Node) * nodes);// 建立node的array
     int id, mem_cnt;
-    for(int i = 0; i < nodes; i++){
+    for(int i = 0; i < nodes; i++){// 讀入每個node的memory數
         scanf("%d%d", &id, &mem_cnt);
         node_arr[id].mem_cnt = mem_cnt;
-        node_arr[id].channel = (int*)calloc(nodes,sizeof(int));
+        node_arr[id].channel = (int*)calloc(nodes,sizeof(int));// 為每個node建立channel的array
     }
     int first, second, cnt;
-    for(int i = 0; i < links; i++){
+    for(int i = 0; i < links; i++){// 讀入node之間的channel數
         scanf("%d%d%d%d", &id, &first, &second, &cnt);
         node_arr[first].channel[second] = cnt;
         node_arr[second].channel[first] = cnt; 
     }
+
     int * req_start = (int*)malloc(sizeof(int) * requests);
     int * req_end = (int*)malloc(sizeof(int) * requests);
-    for(int i = 0; i < requests; i++){
+    for(int i = 0; i < requests; i++){// 讀入request
         scanf("%d%d%d", &id, &req_start[i], &req_end[i]);
     }
 
-    int * queue = (int*)malloc(sizeof(int) * nodes);
+    int * queue = (int*)malloc(sizeof(int) * nodes);// 建立queue
     int front, back;// front為queue最前面元素的前一個位置的index, back為queue最後一個元素的index
 
-    char * color_arr = (char*)malloc(sizeof(char) * nodes);
-    int * predecessor_arr = (int*)malloc(sizeof(int) * nodes);
-    int * length_arr = (int*)malloc(sizeof(int) * nodes);
+    char * color_arr = (char*)malloc(sizeof(char) * nodes);// 紀錄node的顏色
+    int * predecessor_arr = (int*)malloc(sizeof(int) * nodes);// 紀錄node被哪個node找到
+    int * length_arr = (int*)malloc(sizeof(int) * nodes);// 紀錄起點到node的路徑長
 
-    int ** path_arr = (int**)calloc(requests, sizeof(int*));// 將可以用的path存起來
-    int * accept_arr = (int*)calloc(requests, sizeof(int));// 紀錄那些request為accepted
+    int ** path_arr = (int**)calloc(requests, sizeof(int*));// 儲存accept request的路徑
+    int * accept_arr = (int*)calloc(requests, sizeof(int));// 紀錄哪些request為accepted
     int accept_cnt = 0;// 紀錄accept request的數量    
 
     for(int i = 0; i < requests; i++){
@@ -60,12 +59,12 @@ int main(){
                 if(node_arr[queue[front + 1]].channel[j]){
                     if(color_arr[j] == 'w'){
                         color_arr[j] = 'g';
-                        length_arr[j] = length_arr[queue[front + 1]] + 1;;
+                        length_arr[j] = length_arr[queue[front + 1]] + 1;
                         predecessor_arr[j] = queue[front + 1];
                         queue[++back] = j;
                     }else if(color_arr[j] == 'g'){
-                        if(length_arr[j] == length_arr[queue[front + 1]] + 1){
-                            int min_resource1 = (node_arr[predecessor_arr[j]].channel[j] < (node_arr[predecessor_arr[j]].mem_cnt / 2)) ? node_arr[predecessor_arr[j]].channel[j] : (node_arr[predecessor_arr[j]].mem_cnt / 2);
+                        if(length_arr[j] == length_arr[queue[front + 1]] + 1){// 若到達下一個Node的路徑長一樣，則比較兩條路徑的資源多寡，取資源較多者的路徑
+                            int min_resource1 = (node_arr[predecessor_arr[j]].channel[j] < (node_arr[predecessor_arr[j]].mem_cnt)) ? node_arr[predecessor_arr[j]].channel[j] : (node_arr[predecessor_arr[j]].mem_cnt);
                             int min_resource2 = (node_arr[queue[front + 1]].channel[j] < node_arr[queue[front + 1]].mem_cnt) ? node_arr[queue[front + 1]].channel[j] : node_arr[queue[front + 1]].mem_cnt;
                             if(min_resource1 < min_resource2)
                             predecessor_arr[j] = queue[front + 1];
@@ -76,7 +75,7 @@ int main(){
             color_arr[queue[front + 1]] = 'b';
             front++;
         }
-        if(predecessor_arr[req_end[i]] != -1){
+        if(predecessor_arr[req_end[i]] != -1){// 判斷是否有路徑通往終點，有的話才執行if內的程式碼
             int path_len = 1;
             int current = req_end[i];
             while(current != req_start[i]){// 計算path的長度
@@ -87,17 +86,16 @@ int main(){
             for(int j = 0; j < nodes; j++){
                 path[j] = -1;
             }
+
             // 檢查shortest path可不可行
             int accept = 1;
             current = predecessor_arr[req_end[i]];// end的前一個node
             if(node_arr[req_end[i]].mem_cnt == 0 || node_arr[req_start[i]].mem_cnt == 0){
                 accept = 0;
-                // printf("%d:front\n", i);
             }else{
                 while(predecessor_arr[current] != -1){
                     if(node_arr[current].mem_cnt < 2){
                         accept = 0;
-                        // printf("%d:back, cnt: %d, id: %d\n", i, node_arr[current].mem_cnt, current);
                         break;
                     }
                     current = predecessor_arr[current];
